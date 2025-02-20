4414fa02f69bb98d3728cfa766ec2a3bbb3805b6
Kshitij Chakvarty <chakravartykshitij@gmail.com>
2025-02-20 15:57:01 -0800

    Update config path

--- src/rebalancer/rebalancer.py
+++ src/rebalancer/rebalancer.py
@@ -24,7 +24,7 @@ def get_gmail_service():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('config/credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
